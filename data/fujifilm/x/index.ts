2821b6ed85563271d6193815d99fd100152d59b1
isamu <isamu@to-kyo.to>
2023-06-26 06:30:28 +0900

    XF50mm F1.0 R WR

--- data/fujifilm/x/index.ts
+++ data/fujifilm/x/index.ts
@@ -19,4 +19,22 @@ export const fujifilmXLensDataSet: LensData[] = [
       kakaku: "https://kakaku.com/item/K0001302657/",
     },
   },
+  {
+    id: "4547410435856",
+    EANCode: "4547410435856",
+    maker: "Fujifilm",
+    name: "XF50mmF1.0 R WR",
+    mount: "X",
+    focalLength: [50],
+    fStop: [1.0],
+    format: "APS-C",
+    focus: ["AF", "MF"],
+    hasStabilizer: false,
+    hasDustMoistureResistance: true,
+    filterDiameter: 77,
+    weight: 845,
+    urls: {
+      kakaku: "https://kakaku.com/item/K0001286475/",
+    },
+  },
 ];
--- docs/fujifilm/x/index.md
+++ docs/fujifilm/x/index.md
@@ -15,3 +15,19 @@
 | Official URL |  |
 | URL | [kakaku](https://kakaku.com/item/K0001302657/) |
 | EAN/JAN Code | 4547410437881 |
+
+### XF50mmF1.0 R WR
+|  | |
+| -- | -- |
+| Mount  | X mount |
+| Focus length | 50mm |
+| FStop | F1 |
+| Format  | APS-C |
+| Image Stabilizer  |   |
+| Dust / Moisture Resistance | YES  |
+| Filter Diameter | 77mm |
+| weight | 845g |
+| Release Date |  |
+| Official URL |  |
+| URL | [kakaku](https://kakaku.com/item/K0001286475/) |
+| EAN/JAN Code | 4547410435856 |
