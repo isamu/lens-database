94819c91ee16a5f3145d47f2eab4276b94000d82
isamu <isamu@to-kyo.to>
2023-06-25 12:27:04 +0900

    update type

--- data/types/index.ts
+++ data/types/index.ts
@@ -1,11 +1,24 @@
-type Maker = "Canon";
+export type Maker = "Canon";
 
-type Mount = "RF" | "EF" | "EF-S";
+export type Mount = "RF" | "EF" | "EF-S";
+
+export type Format = "Large" | "Full-Frame" | "APS-C" | "FourThirds";
+
+export type Focus = "AF" | "MF";
+
 
 export type LensData = {
-  id: string;
+  id: string; // EAN/JAN code
   maker: Maker;
   name: string;
   mount: Mount;
   focalLength: number[];
+  fStop: number[];
+  format?: Format;
+  focus: Focus[];
+  hasStabilizer: boolean;
+  hasDustMoistureResistance: boolean;
+  filterDiameter: number;
+  weight: numner;
+  url: string;
 };
